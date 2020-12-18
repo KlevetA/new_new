03a6b2779f006dcf068c5c1e5f36b70903c8d45f
user <tolbin.ag@gmail.com>
2020-12-18 19:52:56 +0300

    last

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,21 +9,23 @@ public class Main {
     public static void main(String[] args) {
         UserDaoJDBCImpl sdf = new UserDaoJDBCImpl();
 
-        sdf.dropUsersTable();
+        //sdf.dropUsersTable();
 
         sdf.createUsersTable();
         String name1 = "vays1";
         String name2 = "vays2";
         String name3 = "vays3";
+        String name4 = "vays4";
         sdf.saveUser(name1,"asfasf", (byte) 23);
         System.out.println("User с именем – " + name1 + " добавлен в базу данных");
         sdf.saveUser(name2,"sddgs", (byte) 77);
         System.out.println("User с именем – " + name2 + " добавлен в базу данных");
         sdf.saveUser(name3,"shree", (byte) 8);
         System.out.println("User с именем – " + name3 + " добавлен в базу данных");
+        sdf.saveUser(name4,"asfasf", (byte) 23);
+        System.out.println("User с именем – " + name4 + " добавлен в базу данных");
         List<User> sddsgs = sdf.getAllUsers();
         System.out.println(sddsgs.toString()); //должен быть переопределен toString в классе User
-        sdf.createUsersTable();
-        //sdf.dropUsersTable();
+        sdf.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,30 +2,38 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        d.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        d.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        d.saveUser(name,lastName,age);
     }
 
     public void removeUserById(long id) {
-
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        d.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        return d.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        UserDaoJDBCImpl d = new UserDaoJDBCImpl();
+        d.cleanUsersTable();
     }
 }
